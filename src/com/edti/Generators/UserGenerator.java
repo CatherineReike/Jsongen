1f2078769fbd569f84048dff4b1e5bbba04e7d2b
CatherineReike <reike@stud.uni-obuda.hu>
2022-07-05 10:33:41 +0200

    HashMaps

--- src/com/edti/Generators/UserGenerator.java
+++ src/com/edti/Generators/UserGenerator.java
@@ -9,6 +9,32 @@ import java.util.HashMap;
 
 public class UserGenerator implements IUserGenerator {
 
+    public static HashMap<Integer, String> Veznev;
+    static {
+        Veznev = new HashMap<>();
+        Veznev.put(0, "Kovács");
+        Veznev.put(1, "Nemes");
+        Veznev.put(2, "Kis");
+        Veznev.put(3, "Nagy");
+        Veznev.put(4, "Tamási");
+        Veznev.put(5, "Kelemen");
+        Veznev.put(6, "Kelemen");
+    }
+
+    public static HashMap<Integer, String> Kernev;
+    static {
+        Veznev = new HashMap<>();
+        Veznev.put(0, "Imre");
+        Veznev.put(1, "János");
+        Veznev.put(2, "Mátyás");
+        Veznev.put(3, "Dóra");
+        Veznev.put(4, "Anna");
+        Veznev.put(5, "Ágnes");
+        Veznev.put(6, "Béla");
+    }
+
+
+
     @Override
     public Collection<User> generate() {
         return null;
@@ -28,4 +54,6 @@ public class UserGenerator implements IUserGenerator {
     public String generateEmail() {
         return null;
     }
+
+    
 }
