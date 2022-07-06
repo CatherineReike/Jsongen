9903c7ccb7559ec4289c765a158f193d1dddeb3d
vasvince98 <vas.vince98@gmail.com>
2022-07-06 13:27:53 +0200

    correct sdk

--- src/com/edti/Main.java
+++ src/com/edti/Main.java
@@ -9,12 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         IUserGenerator test = new UserGenerator();
-        //List<User> users = new ArrayList<>(test.generateUsers());
-        //NeptunStudentWrapper wrapper = new NeptunStudentWrapper();
-        //wrapper.setOE_AktivJogviszonyosHallgatokAdat(users);
 
-
-        //ez itten a User Generatorban történjen meg
         test.GODgenerate();
     }
 }
