5aeb8e33d316af0bf80e763856b1de7cb7b79c1b
CatherineReike <reike@stud.uni-obuda.hu>
2022-07-06 13:15:34 +0200

    Everything in one commit

--- src/com/edti/Generators/UserGenerator.java
+++ src/com/edti/Generators/UserGenerator.java
@@ -1,15 +1,23 @@
 package com.edti.Generators;
 
 
+import com.edti.Interfaces.IGlobalSerialize;
 import com.edti.Interfaces.IUserGenerator;
 
 import com.edti.Models.User;
+import com.edti.Shared.GlobalSerialize;
 import com.edti.Shared.ParamLoader;
+import com.edti.Wrappers.NeptunStudentWrapper;
+import com.edti.Wrappers.NeptunTeacherWrapper;
 
+import java.io.IOException;
 import java.util.*;
 
 public class UserGenerator implements IUserGenerator {
+
     Random r = new Random();
+    NeptunStudentWrapper neptunStudentWrapper = new NeptunStudentWrapper();
+    NeptunTeacherWrapper neptunTeacherWrapper = new NeptunTeacherWrapper();
 
     public static HashMap<Integer, String> Veznevs;
     static {
@@ -36,11 +44,9 @@ public class UserGenerator implements IUserGenerator {
     }
 
     private int numberOfStudents;
-    private int numberOfTeachers;
-
 
     @Override
-    public Collection<User> generateStudents() {
+    public Collection<User> generateUsers() {
         loadExternalParams(ParamLoader.getParams("data.txt"));
         Set<User> creativeUserSet = new HashSet<>();
         int i =0;
@@ -50,41 +56,17 @@ public class UserGenerator implements IUserGenerator {
             String vN = generateVezNev();
             String nyeptun = generateNeptuneId();
             User currentUser = new User(r.nextInt(10000000), nyeptun, vN, kN, String.format("%s %s", vN, kN), generateEmail(nyeptun));
-            System.out.println(currentUser.getUserLastChanged());
             if (!creativeUserSet.contains(currentUser)) {
                 creativeUserSet.add(currentUser);
                 i++;
             }
         }
-
-        return creativeUserSet;
-    }
-
-    @Override
-    public Collection<User> generateTeachers() {
-        loadExternalParams(ParamLoader.getParams("data.txt"));
-        Set<User> creativeUserSet = new HashSet<>();
-        int i =0;
-        while(i != numberOfTeachers)
-        {
-            String kN = generateKerNev();
-            String vN = generateVezNev();
-            String nyeptun = generateNeptuneId();
-            User currentUser = new User(r.nextInt(10000000), nyeptun, vN, kN, String.format("%s %s", vN, kN), generateEmail(nyeptun));
-            System.out.println(currentUser.getUserLastChanged());
-            if (!creativeUserSet.contains(currentUser)) {
-                creativeUserSet.add(currentUser);
-                i++;
-            }
-        }
-
         return creativeUserSet;
     }
 
     @Override
     public void loadExternalParams(HashMap<String, String> params) {
         this.numberOfStudents = Integer.parseInt(params.get("numberOfStudents"));
-        this.numberOfTeachers = Integer.parseInt(params.get("numberOfTeachers"));
     }
 
 
@@ -124,11 +106,44 @@ public class UserGenerator implements IUserGenerator {
         String Nev ="";
         Random r = new Random();
         Nev = Kernevs.get(r.nextInt(Kernevs.size()));
-
         return Nev;
-
-
     }
 
+    public HashMap<String, ArrayList<User>> GODgenerate() throws IOException {
+
+        Collection<User> users = new HashSet<>(generateUsers());
+        ArrayList<User> students = new ArrayList<>();
+        ArrayList<User> teachers = new ArrayList<>();
+        HashMap<String, ArrayList<User>> vinceEmberkei = new HashMap<>();
+        IGlobalSerialize<NeptunStudentWrapper> serializerS = new GlobalSerialize<>();
+        IGlobalSerialize<NeptunTeacherWrapper> serializerT = new GlobalSerialize<>();
+
+        int i =1;
+        //add to wrappers
+        for (User user: users){
+            if (i % 3 == 0){
+                if (i % 4 ==0){
+                    neptunStudentWrapper.getOE_AktivJogviszonyosHallgatokAdat().add(user);
+                    students.add(user);
+
+                }
+                neptunTeacherWrapper.getOE_AlkalmazottakAdat().add(user);
+                teachers.add(user);
+            }
+            else
+                neptunStudentWrapper.getOE_AktivJogviszonyosHallgatokAdat().add(user);
+            students.add(user);
+            i++;
+        }
+
+        vinceEmberkei.put("student", students);
+        vinceEmberkei.put("teacher", teachers);
+
+        serializerS.serialize(neptunStudentWrapper, "OE_Get_AktivJogviszonyosHallgatokAdatok");
+        serializerT.serialize(neptunTeacherWrapper, "OE_Get_AlkalmazottakAdatok");
+
+        return vinceEmberkei;
+                
+    }
 
 }
--- src/com/edti/Interfaces/IGlobalSerialize.java
+++ src/com/edti/Interfaces/IGlobalSerialize.java
@@ -3,5 +3,5 @@ package com.edti.Interfaces;
 import java.io.IOException;
 
 public interface IGlobalSerialize<T> {
-    void serialize(T object) throws IOException;
+    void serialize(T object,  String pathName) throws IOException;
 }
